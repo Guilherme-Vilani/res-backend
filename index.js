4e030535cd99de1aaeda4ad7afc1f70520875b79
Guilherme-Vilani <gui.vilani79@gmail.com>
2021-12-01 12:37:02 -0400

    teste heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
-
+var mongoose = require('mongoose')
 var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://admin:159263gui@clusterrestaurante-shard-00-00.ydy7y.mongodb.net:27017,clusterrestaurante-shard-00-01.ydy7y.mongodb.net:27017,clusterrestaurante-shard-00-02.ydy7y.mongodb.net:27017/Restaurante?authSource=admin&replicaSet=atlas-5pzkbb-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true');
 
 app.get('/', (req, res) => {
   res.send('Servidor Restaurante no ar!!!')
